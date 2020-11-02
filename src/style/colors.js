bd16797529fdb96265b132202c672294efd587e3
bacht <bachgtung144007@gmail.com>
2020-11-02 21:32:37 +0700

    fix base color

--- src/style/colors.js
+++
@@ -1,7 +0,0 @@
-export const WHITE = '#FFFFFF';
-export const CHELSEA_CUCUMBER = '#7CB342';
-export const GAINSBORO = '#E1E1E1';
-export const DIM_GRAY = '#707070';
-export const ZAMBEZI = '#5F5F5F';
-
-
