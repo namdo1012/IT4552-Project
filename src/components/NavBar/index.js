dbd5a833f557728f64d16e620222aa434bcd10db
namdo1012 <namdo10121999@gmail.com>
2020-12-17 18:31:51 +0700

    Sửa component Navbar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -4,29 +4,62 @@ import React from "react";
 import "./style.css";
 import { Link } from "react-router-dom";
 
-export const NavBar = () => {
+export const NavBar = ({ style }) => {
   return (
-    <Navbar className="app__navbar" collapseOnSelect expand="lg" variant="dark">
-      <Navbar.Brand href="#home" className="app__logo">
-        <BsFillBrightnessHighFill className="app__icon" />
+    <Navbar
+      style={{ ...style }}
+      className="app__navbar"
+      collapseOnSelect
+      expand="lg"
+      variant="dark"
+    >
+      <Navbar.Brand
+        href="#home"
+        className="app__logo"
+        style={{ color: style.color }}
+      >
+        <BsFillBrightnessHighFill
+          className="app__icon"
+          style={{ color: style.color }}
+        />
         BT Nihongo
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mx-auto app__navbar-list">
-          <Nav.Link href="/course" className="app__navbar-link">
+          <Nav.Link
+            style={{ color: style.color }}
+            href="/course"
+            className="app__navbar-link"
+          >
             Khóa học
           </Nav.Link>
-          <Nav.Link href="/videos" className="app__navbar-link">
+          <Nav.Link
+            style={{ color: style.color }}
+            href="/videos"
+            className="app__navbar-link"
+          >
             Giải trí
           </Nav.Link>
-          <Nav.Link href="#pricing" className="app__navbar-link">
+          <Nav.Link
+            style={{ color: style.color }}
+            href="#pricing"
+            className="app__navbar-link"
+          >
             Blog
           </Nav.Link>
-          <Nav.Link href="/references" className="app__navbar-link">
+          <Nav.Link
+            style={{ color: style.color }}
+            href="/references"
+            className="app__navbar-link"
+          >
             Tài liệu
           </Nav.Link>
-          <Nav.Link href="#pricing" className="app__navbar-link">
+          <Nav.Link
+            style={{ color: style.color }}
+            href="#pricing"
+            className="app__navbar-link"
+          >
             Luyện thi
           </Nav.Link>
         </Nav>
@@ -35,6 +68,10 @@ export const NavBar = () => {
           <Button
             variant="outline-secondary"
             className="app__button nav__button--login"
+            style={{
+              color: style.color,
+              outline: `${style.outlineColor} solid 2px`,
+            }}
           >
             Đăng Nhập
           </Button>
