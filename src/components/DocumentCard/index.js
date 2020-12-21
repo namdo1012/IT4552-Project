83641d0abcded1d69ec6ecf143dc1936f7fe50e9
namdo1012 <namdo10121999@gmail.com>
2020-12-21 13:44:18 +0700

    Hoàn thành giao diện trang tài liệu

--- src/components/DocumentCard/index.js
+++ src/components/DocumentCard/index.js
@@ -4,16 +4,27 @@ import { FaDownload } from "react-icons/fa";
 const DocumentCard = ({ cover, title, linkDownload }) => {
   return (
     <div className="col mb-4">
-      <div className="card rounded" style={{ maxWidth: "540px" }}>
+      <div
+        className="card rounded references__card"
+        style={{ maxWidth: "540px" }}
+      >
         <div className="row no-gutters">
-          <div className="col-sm-4">
-            <img src={cover} className="card-img p-3" alt={title} />
+          <div className="col-sm-5">
+            <img
+              src={cover}
+              className="card-img p-3"
+              alt={title}
+              style={{ height: "100%" }}
+            />
           </div>
-          <div className="col-sm-8">
+          <div className="col-sm-7">
             <div className="card-body">
-              <h5 className="card-title">{title}</h5>
+              <h5 className="references__card-title">{title}</h5>
               <a href={linkDownload} target="_blank" rel="noreferrer">
-                <button type="button" className="btn btn-success mt-4">
+                <button
+                  type="button"
+                  className="btn btn-success references__button--download"
+                >
                   <FaDownload className="mr-1" />
                   Tải về
                 </button>
