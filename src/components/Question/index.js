f4911957e3802b4c82854838e68e12addcb7c73b
bacht <bachgtung144007@gmail.com>
2020-12-23 16:05:24 +0700

    set value answer

--- src/components/Question/index.js
+++ src/components/Question/index.js
@@ -1,60 +1,37 @@
-import React from "react";
+import React, {useState} from "react";
 import "./style.css";
 
-const Question = ({ id, question }) => {
+const Question = ({ id, question, setAnswer, answer }) => {
+
+  // const check = (value) => {
+  //   if (answer.find(item => item.id === id++)) {
+  //     let tmp = answer.filter(item => item.id !== id++)
+  //     setAnswer([...tmp,{id:id++,value:value}])
+  //   }
+  //   else setAnswer([...answer,{id:id++,value:value}])
+  // }
   return (
     <div className="question">
       <div className="question__title">Câu hỏi {id + 1}</div>
       <p className="question__content">{question.title}</p>
       <div className="answer">
-        <div className="form-check">
-          <input
-            className="form-check-input"
-            type="radio"
-            name={id + "answer"}
-            value="A"
-            id={id + "A"}
-          />
-          <label className="form-check-label" htmlFor={id + "A"}>
-            {question.answer.A}
-          </label>
-        </div>
-        <div className="form-check">
-          <input
-            className="form-check-input"
-            type="radio"
-            name={id + "answer"}
-            value="B"
-            id={id + "B"}
-          />
-          <label className="form-check-label" htmlFor={id + "B"}>
-            {question.answer.B}
-          </label>
-        </div>
-        <div className="form-check">
-          <input
-            className="form-check-input"
-            type="radio"
-            name={id + "answer"}
-            value="C"
-            id={id + "C"}
-          />
-          <label className="form-check-label" htmlFor={id + "C"}>
-            {question.answer.C}
-          </label>
-        </div>
-        <div className="form-check">
-          <input
-            className="form-check-input"
-            type="radio"
-            name={id + "answer"}
-            value="D"
-            id={id + "D"}
-          />
-          <label className="form-check-label" htmlFor={id + "D"}>
-            {question.answer.D}
-          </label>
-        </div>
+        {
+          question?.answer.map(item => (
+              <div className="form-check" key={item?.type}>
+                <input
+                    className="form-check-input"
+                    type="radio"
+                    name={`id${item?.content}`}
+                    value={item?.type}
+                    id={`id${item?.type}`}
+                    // onChange={e => check(e.target.value)}
+                />
+                <label className="form-check-label" htmlFor={`id${item?.type}`}>
+                  {item?.content}
+                </label>
+              </div>
+          ))
+        }
       </div>
     </div>
   );
