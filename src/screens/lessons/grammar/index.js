c729273c2f6e60ec17396e54b66124b6264a856e
huy-xu <nhathuyhedspi@gmail.com>
2020-12-26 10:38:03 +0700

    Get data from firebase

--- src/screens/lessons/grammar/index.js
+++ src/screens/lessons/grammar/index.js
@@ -1,26 +1,22 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from "react";
 import "./style.css";
+import * as ROUTES from "../../../constant/routes";
 import { BackBtn } from "../../../components/backBtn";
 
-import GrammarImage from "./grammar-img.png";
-
 export const Grammar = ({ doneLesson, dataLesson }) => {
-  console.log("data lesson", dataLesson);
   return (
     <>
       <div className="header">
-        <BackBtn style={{ marginTop: "50px" }} dest="/course">
+        <BackBtn style={{ marginTop: "50px" }} dest={ROUTES.COURSE}>
           {" "}
           Back to course{" "}
         </BackBtn>
       </div>
       <div className="content container-fluid text-center">
-        <h3 className="inline-block mb-3">
-          {dataLesson?.title}謙譲語 (Khiêm nhường ngữ)
-        </h3>
+        <h3 className="inline-block mb-3">{dataLesson?.title}</h3>
         <div className="img-content">
-          <img src={GrammarImage} className="" alt={dataLesson?.title} />
+          <img src={dataLesson?.url} className="" alt={dataLesson?.title} />
         </div>
       </div>
       <div className="done-bar fixed-bottom d-flex justify-content-center">
